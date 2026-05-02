ad8a0cd34533bf0abca55dbe980d6c791526a9dc
Will Butler <butler.will1@gmail.com>
2026-05-02 12:24:34 +0100

    delete project

--- src/pipeline_qa_extractor/__init__.py
+++
@@ -1,7 +0,0 @@
-# Package exports for the Stage 1 pipeline QA extractor.
-"""Public package surface for the Stage 1 extractor implementation."""
-__all__ = [
-    "cli",
-    "extractor",
-    "models",
-]
